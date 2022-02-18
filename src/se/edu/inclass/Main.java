2a5e2bfe5352b12cf81a53f100822db2cdd51468
Mathan <mathanmahe99@gmail.com>
2022-02-18 17:44:58 +0800

    Update code to filter data using lambdas

--- src/se/edu/inclass/Main.java
+++ src/se/edu/inclass/Main.java
@@ -7,6 +7,8 @@ import se.edu.inclass.task.TaskNameComparator;
 
 import java.util.ArrayList;
 
+import static java.util.stream.Collectors.toList;
+
 public class Main {
 
     private TaskNameComparator taskNameComparator;
@@ -20,6 +22,11 @@ public class Main {
 
         System.out.println("Total number of deadlines: " + countDeadlines(tasksData));
 
+
+//        printDeadlinesWithStream(tasksData);
+
+        ArrayList<Task> filteredList = filterTasksByString(tasksData,"11");
+        System.out.println(filteredList);
     }
 
     private static int countDeadlines(ArrayList<Task> tasksData) {
@@ -38,11 +45,32 @@ public class Main {
         }
     }
 
+    public static void printDeadlinesWithStream(ArrayList<Task> tasks) {
+        System.out.println("printing deadlines with streams sorted");
+        tasks.stream()
+                .filter((t)-> t instanceof Deadline)
+                .sorted((a,b)-> a.getDescription().toLowerCase().compareTo(b.getDescription().toLowerCase()))
+                .forEach(System.out::println);
+        //the lambda here takes two parameters to compare the two descriptions
+
+    }
+
     public static void printDeadlines(ArrayList<Task> tasksData) {
         for (Task t : tasksData) {
             if (t instanceof Deadline) {
                 System.out.println(t);
             }
         }
+    }
+
+    private static ArrayList<Task> filterTasksByString(ArrayList<Task> tasks, String s) {
+        System.out.println("Printing filteredlist of data");
+        ArrayList<Task> filteredList = (ArrayList<Task>) tasks.stream()
+                .filter((t)-> t.getDescription().contains(s))
+                .collect(toList());
+        //collect returns a list but we need arraylist of type task so we cast it as such
+        //collect function will collect those tsaks in stream
+
+        return filteredList;
     }
 }
